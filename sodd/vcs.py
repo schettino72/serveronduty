461c710562e25782aa8ee148e988a21cd26d02b2
Eduardo Schettino <schettino72@gmail.com>
2009-11-03 21:54:42 +0800

    sodd: added support for bzr

--- sodd/vcs.py
+++ sodd/vcs.py
@@ -5,28 +5,31 @@ import subprocess
 
 
 class SVN(object):
-    def __init__(url, work_path):
+    def __init__(self, url, work_path):
         self.url = url
         self.work_path = work_path
+
 
     def clone(self):
         """SVN checkout"""
 	if not os.path.exists(self.work_path):
             subprocess.call(['svn', 'checkout', self.url, self.work_path])
 
+
     def export(self, rev_num, dst_path):
         subprocess.call(['svn', 'export', '--force', '-r', rev_num,
                          self.work_path, dst_path])
 
+
     def fetch(self):
         subprocess.call(['svn', 'update', self.work_path])
+
 
     def get_new_revisions(self, from_rev):
         # first update working copy
         self.fetch()
-        log_proc = subprocess.Popen(['svn','log', '-q',
-                                     '-r', '%s:HEAD' % from_rev])
-
+        cmd = ['svn','log', '-q', '-r', '%s:HEAD' % from_rev, self.work_path]
+        log_proc = subprocess.Popen(cmd)
         out = log_proc.communicate()[0]
         revs = []
         for line in out:
@@ -38,7 +41,45 @@ class SVN(object):
         return revs[1:]
 
 
+
+class BZR(object):
+    def __init__(self, url, work_path):
+        self.url = url
+        self.work_path = work_path
+
+
+    def clone(self):
+        """SVN checkout"""
+	if not os.path.exists(self.work_path):
+            subprocess.call(['bzr', 'branch', self.url, self.work_path])
+
+
+    def export(self, rev_num, dst_path):
+        subprocess.call(['bzr', 'export', '--revision=%s' % rev_num,
+                         dst_path, self.work_path])
+
+
+    def fetch(self):
+        subprocess.call(['bzr', 'pull', '--directory=%s' % self.work_path])
+
+
+    def get_new_revisions(self, from_rev):
+        # first update working copy
+        self.fetch()
+        cmd = ['bzr','log','--line','--revision=%s..'%from_rev, self.work_path]
+        log_proc = subprocess.Popen(cmd)
+        out = log_proc.communicate()[0]
+        revs = []
+        for line in out:
+            # 174: eduardo 2009-10-30 xxx yyy zzz
+            revs.append(line.split()[0][:-1]) # get first word and remove ':'
+        # exclude old tip/head
+        return revs[1:]
+
+
+
 def get_vcs(vcs_name, url):
     """return a VCS object"""
-    vcs_map = {'svn': SVN}
+    vcs_map = {'svn': SVN,
+               'bzr': BZR}
     return vcs_map[vcs_name](url)
